dbd492c8e11f85727ccd18130f8a5ecd919351b0
kattak <katrina.devaney@gmail.com>
2016-10-24 05:48:10 -0700

    Added writeup of Puppy class in puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------------
+# Yorkshire Terrier
+
+# CHARACTERISTICS ----------------
+# Eyelash length: varies
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: black and gold
+
+# BEHAVIOR -----------------------
+# Jump
+# Cuddle
+# Sleep
