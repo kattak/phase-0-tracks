c2495aae31c6c0f69040c52dc50b53e3ac880dcb
kattak <katrina.devaney@gmail.com>
2016-10-24 11:33:04 -0700

    Added function longest word and driver code

--- js/algos.js
+++ js/algos.js
@@ -16,6 +16,40 @@ function longest_word(ary){
   return result
 }
 
+//function returns true/false if the objects share at least one key-value pair
+//get list of keys
+//set "match" bool to false
+//iterate through the keys of the objects, checking if key-value pairs match
+//if at least one matches, set match to true
+//return "match"
+
+function one_match(obj1, obj2){
+  match = false;
+
+  keys = Object.keys(obj1);
+
+  for (var i = 0; i < keys.length; i++){
+    current_key = keys[i];
+    val1 = obj1[current_key];
+    val2 = obj2[current_key];
+    if (val1 == val2){
+      match = true;
+    }
+  }
+  return match
+}
+
+
+
 //Driver Code
+
+//Longest Word function returns longest element of an array
 console.log(longest_word(["long phrase","longest phrase","longer phrase"]))
-//should return "longest phrase"
+//=> should return "longest phrase"
+
+//One Match function returns true/false if there's at least one key value pair match
+console.log(one_match({name: "Steven", age: 54}, {name: "Tamir", age: 54}))
+//=> should return true
+
+console.log(one_match({name: "Steven", age: 54}, {name: "Tamir", age: 53}))
+//=> should return false
