06375efbccca0e8482e482fce613d512d3a75950
kattak <katrina.devaney@gmail.com>
2016-10-24 07:52:31 -0700

    shout.rb has two classes, ole_yeller, yung_yeller, accessing shout mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,20 +1,36 @@
 module Shout
-  def self.yell_angrily(words)
+
+  def yell_angrily(words) # use self.method when writing module as standalone module
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily
-    return "yay!!"
+  def yelling_happily #don't use self. when writing module as a mixin class
+    "yay!!"
   end
 
 end
 
+class Ole_Yeller
+  include Shout
+end
+
+class Yung_Yeller
+  include Shout
+end
+
 #Driver Code
-#To run driver code, load file in irb after running require_relative 'shout'
+#To run driver code lines 20 - 26, load file in irb after running require_relative 'shout'
 
-#Yell angrily
-Shout.yell_angrily("bah")
+# #Yell angrily
+# Shout.yell_angrily("bah")
 
-#Yell happily
-Shout.yelling_happily
+# #Yell happily
+# Shout.yelling_happily
 
+
+######
+grandpa = Ole_Yeller.new
+grandpa.yell_angrily("bah")
+
+grandson = Yung_Yeller.new
+grandson.yelling_happily
