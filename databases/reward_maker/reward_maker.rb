d529c7c72ee2d02777ee722faf8ca2bb8a084e03
kattak <katrina.devaney@gmail.com>
2016-11-07 09:58:39 -0800

    Made sample reward_maker that generates 100 sample rewards (for a game)

---
+++ databases/reward_maker/reward_maker.rb
@@ -0,0 +1,39 @@
+
+# require gems
+require 'sqlite3'
+require 'faker'
+
+# create SQLite3 database
+db = SQLite3::Database.new("rewards.db")
+db.results_as_hash = true
+
+# learn about fancy string delimiters
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS rewards(
+    id INTEGER PRIMARY KEY,
+    color VARCHAR(255),
+    points INT
+  )
+SQL
+
+# create a rewards table (if it's not there already)
+db.execute(create_table_cmd)
+
+# add a test reward
+# db.execute("INSERT INTO rewards (color, points) VALUES ('yellow', 10)")
+
+# add multiple rewards
+def create_kitten(db, color, points)
+  db.execute("INSERT INTO rewards (color, points) VALUES (?, ?)", [color, points])
+end
+
+100.times do
+  create_reward(db, Faker::Name.color, 0)
+end
+
+# explore ORM by retrieving data
+# rewards = db.execute("SELECT * FROM rewards")
+# rewards.each do |reward|
+#  puts "reward, color #{reward['color']} is #{reward['points']} points"
+# end
+
