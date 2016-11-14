c28713b3d887ff0c37213b830e6ffba93fb32ff5
kattak <katrina.devaney@gmail.com>
2016-11-14 01:04:20 -0800

    9.4 added routes to web application built in ruby

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,26 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+
+# write a /contact route that displays an address
+get '/:person/contact' do
+  person = params[:person]
+  "#{params[:address]}"
+end
+
+# A /great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!". If the query parameter is not present, the route simply says "Good job!"
+
+get '/great_job/:person' do
+  name = params[:name]
+  if name
+  "Great job, #{name}"
+else
+  "Good job!"
+end
+end
+
+# A route that uses route parameters to add two numbers and respond with the result. The data types are tricky here -- when will the data need to be (or arrive as) a string?
+get '/:number_1/adds/:number_2' do
+  "#{params[:number_1]}.to_i + #{params[:number_2]}.to_i"
+end
